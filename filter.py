ca4c4bb3480f56962bcbec24ed938049058cebe6
jstray <jonathanstray@gmail.com>
2017-07-13 14:20:33 -0700

    First pass

---
+++ filter.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import wf
+class Importable:
+  @staticmethod
+
+  @staticmethod
+  def render(wf_module, table):
+    if table is None:
+      return None     # no rows to process
+
+    col = wf_module.get_param('column').strip()
+    value = float(wf_module.get_parm('value'))
+
+    colnames = list(table.columns)
+    if col in colnames:
+        newtab = table[table[col]>value]
+        
+    return table
+
